e0f4ec87e6c623490ec5287121c8fd832aef8968
Andrei Gerasimchuk <gerasimchuk.grodno@gmail.com>
2019-08-13 18:03:45 +0300

    init start react app

---
+++ src/containers/list-data-container.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class ListDataContainer extends Component {
+  state = {
+    list: [],
+  };
+
+  render() {
+    return (
+      'test'
+    );
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ListDataContainer } from './containers/list-data-container';
+
+const App = () => (<ListDataContainer />);
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
